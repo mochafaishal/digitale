9e9d22f0dc415b4eeac4b32e089d831fe914ce04
mochafaishal <mochammadfaishal21@gmail.com>
2018-04-13 09:27:52 -0700

    edit

---
+++ index.php
@@ -0,0 +1,271 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Digitale</title>
+  <link rel="shortcut icon" href="images/logo.png">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
+  <link rel="stylesheet" href="theme.css" type="text/css"> </head>
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<body>
+  <nav class="navbar navbar-expand-md bg-primary navbar-dark">
+    <div class="container_header">
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar2SupportedContent" aria-controls="navbar2SupportedContent" aria-expanded="false"
+        aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+      <div class="collapse navbar-collapse text-center justify-content-end" id="navbar2SupportedContent">
+        <ul class="navbar-nav">
+          <li style="padding-right: 760px;"><a class="navbar-brand" href="#"><img src="images/logo.png" alt="" ></i><b> Digitale</b></a>
+          </li>
+          <li class="nav-item">
+            <a href="https://play.google.com/store/apps/details?id=id.digitale.mobile"class="btn navbar-btn ml-2 text-white"><i class=""></i>  Download</a>
+          </li>
+          <li>
+            <a href="publish.php" class="btn navbar-btn ml-2 text-white"><i class=""></i> Publish</a>
+          </li>
+          <li>
+            <li data-toggle="modal" data-target="#mylogin"><a href="#" class="btn navbar-btn ml-2 text-white"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+          </li>
+          <li>
+            <li data-toggle="modal" data-target="#mydaftar"><a href="#" class="btn navbar-btn ml-2 text-white"><span class="glyphicon glyphicon-log-in"></span> Register</a></li>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class="text-left py-5 bg-primary" style="background-position: left center;">
+    <div class="row2">
+      <div class="row">
+        <div>
+          <h1 class="mb-4 text-light display-4">Empower people to educate the world through Entertainment.</h1>
+          <p class="lead mb-5 display-8">Pertama, aplikasi layanan membaca cerita bergambar (cergam) anak di Indonesia yang memberdayakan kreator untuk berkarya secara produktif, dan membantu user mendapatkan pendidikan dengan cara menyenangkan. </p>
+          <a href="https://play.google.com/store/apps/details?id=id.digitale.mobile" class="btn btn-lg mx-1 btn-secondary">Download</a>
+          <a href="publish.php" class="btn btn-lg btn-primary mx-1" > <span class="glyphicon glyphicon-user"></span> Publish Story</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="py-5 bg-light">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 text-center">
+          <h2 class="pb-4 text-primary">Features from the future</h2>
+        </div>
+      </div>
+      <div class="row">
+        <div class="align-self-center text-md-right text-center col-md-4">
+          <h3 class="text-primary">Edutainment Stories</h3>
+          <p class="mb-5">Tersedia cerita-cerita bergambar yang
+            <br>menginspirasi dan menghibur untuk segala usia.</p>
+          <h3 class="text-primary">Scrolling Stories</h3>
+          <p class="mb-5">Enak dibaca ketika sendirian, tinggal scroll ke bawah dan ke atas.</p>
+          <h3 class="text-primary">Sliding Stories</h3>
+          <p class="">Juga asik untuk dibaca bersama-sama dengan lebih jelas, dan nyaman.
+        </div>
+        <div class="my-3 col-md-4">
+          <img class="img-fluid d-block ml-1 mx-auto" src="images/hp1.png"> </div>
+        <div class="align-self-center text-md-left text-center col-md-4">
+          <h3 class="text-primary">Catogories</h3>
+          <p class="mb-5">Menelusuri cerita lebih mudah berdasarkan kategori yang tersedia.</p>
+          <h3 class="text-primary">Story Information</h3>
+          <p class="mb-5">Dapat diketahui sinopsis, penulis dan ilustratornya sebelum cerita mulai untuk dibaca.</p>
+          <h3 class="text-primary">Bookmark</h3>
+          <p>Simpan cerita yang kamu suka, untuk dibaca kembali.
+
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="py-5 text-white bg-primary">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-7 text-md-left text-center align-self-center my-5">
+          <h2 class="display-2">Apa itu Digitale?</h2>
+          <p class="lead">Digitale merupakan layanan membaca buku digital bergambar anak-anak yang membawa nilai-nilai 4E (Entertain, Educate, Empower, & Environmental).</p>
+        </div>
+        <div class="col-md-5">
+          <div class="embed-responsive embed-responsive-4by3  rounded">
+            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/VzUq5fXr934?rel=0" allowfullscreen=""> </iframe>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="py-5 text-center bg-light">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h1>Our Teams</h1>
+          <p class="lead">We grow together with the community</p>
+        </div>
+      </div>
+      <div class="row1">
+        <div class="col-md-3 p-4">
+          <img class="img-fluid d-block rounded-circle mx-auto" src="images/naldi.png" alt="">
+          <p class="my-4"><i>“Cerahnya masa depan, berada pada generasi muda yang berperan pada perubahan. Tugas generasi sekarang adalah mempersiapkan diri sendiri, dan mempersiapkan masa depan penerusnya.“</i></p>
+          <p><b>Rinaldi Madani</b>
+            <br>Chief Operations Officer</p>
+        </div>
+        <div class="col-md-3 p-4">
+        </div>
+        <div class="col-md-3 p-4">
+          <img class="img-fluid d-block rounded-circle mx-auto" src="images/ceo.png" alt="">
+          <p class="my-4"><i>“Cerahnya masa depan, berada pada generasi muda yang berperan pada perubahan. Tugas generasi sekarang adalah mempersiapkan diri sendiri, dan mempersiapkan masa depan penerusnya.“</i></p>
+          <p><b>Najma Qolby</b>
+            <br>Chief Excecutive Officer</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="p-5 bg-primary">
+    <div class="container">
+      <div class="row_iklan">
+        <div class="p-3 align-self-center col-md-4">
+          <div class="card">
+            <div class="card-block p-5">
+              <h1>Rp.0</h1>
+              <h3>Free</h3>
+              <hr>
+              <p>Ada Iklan, dan fitur-fitur yang terbatas.&nbsp;</p>
+              <a href="#" class="btn btn-dark">Subscribe</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="p-3 align-self-center col-md-4">
+          <div class="card">
+            <div class="card-block p-5">
+              <h2>COMMING SOON</h2>
+              <h3>Premium</h3>
+              <hr>
+              <p>Pengalaman pengguna yang tidak terbatas, dan tidak ada iklan.</p>
+              <a href="#" class="btn btn-dark">Subscribe</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="bg-dark text-white">
+    <div class="container">
+      <div class="row">
+        <div class="p-4 col-md-3">
+          <h2 class="mb-4 ">Digitale</h2>
+          <p class="text-white">Aplikasi layanan membaca cerita anak bergambar yang mendidik dan menghibur.</p>
+        </div>
+        <div class="p-4 col-md-3">
+          <h2 class="mb-4 ">Mapsite</h2>
+          <ul class="list-unstyled">
+            <a href="#" class="text-white">Download</a>
+            <br>
+            <a href="#" class="text-white">Publish</a>
+            <br>
+            <a href="#" class="text-white">Login</a>
+            <br>
+            <a href="#" class="text-white">Register</a>
+          </ul>
+        </div>
+        <div class="p-4 col-md-3">
+          <h2 class="mb-4">Contact</h2>
+          <p>
+            <a href="tel:+246 - 542 550 5462" class="text-white"><i class="fa d-inline mr-3 text-secondary fa-phone"></i>-</a>
+          </p>
+          <p>
+            <a href="" class="text-white"><i class="fa d-inline mr-3 text-secondary fa-envelope-o"></i>Redaksi.digitale@gmail.com</a>
+          </p>
+          <p>
+            <a href="https://goo.gl/maps/AUq7b9W7yYJ2" class="text-white" target="_blank"><i class="fa d-inline mr-3 fa-map-marker text-secondary"></i>-</a>
+          </p>
+        </div>
+        <div class="p-4 col-md-3">
+          <h2 class="mb-4 text-light">Subscribe</h2>
+          <form>
+            <fieldset class="form-group text-white"> <label for="exampleInputEmail1">Get our newsletter</label>
+              <input type="email" class="form-control" placeholder="Enter email"> </fieldset>
+            <button type="submit" class="btn btn-outline-secondary">Submit</button>
+          </form>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12 mt-3">
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Modal login -->
+  <div id="mylogin" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title"></h4>
+        </div>
+        <div class="modal-body">
+          <form>
+  		  <div class="form-group">
+  		    <label for="exampleInputEmail1">Username</label>
+  		    <input type="text" class="form-control" id="username" placeholder="Username">
+  		  </div>
+  		  <div class="form-group">
+  		    <label for="exampleInputPassword1">Password</label>
+  		    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  		  </div>
+  		</form>
+        </div>
+        <div class="modal-footer">
+        	<button type="button" class="btn btn-success" data-dismiss="modal">Login</button>
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- End Modal Login -->
+
+  <!-- Modal daftar -->
+<div id="mydaftar" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <h3>Register</h3>
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title"></h4>
+      </div>
+      <div class="modal-body">
+        <form>
+ 		  <div class="form-group">
+		    <label for="exampleInputEmail1">Fullname</label>
+		    <input type="text" class="form-control" id="Fullname" placeholder="Fullname">
+		  </div>
+		  <div class="form-group">
+		    <label for="exampleInputEmail1">Username</label>
+		    <input type="text" class="form-control" id="username" placeholder="Username">
+		  </div>
+		  <div class="form-group">
+		    <label for="exampleInputEmail1">Email address</label>
+		    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+		  </div>
+		  <div class="form-group">
+		    <label for="exampleInputPassword1">Password</label>
+		    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+		  </div>
+		</form>
+      </div>
+      <div class="modal-footer">
+      	<button type="button" class="btn btn-success" data-dismiss="modal">Register</button>
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- end modal daftar -->
+</body>
+
+</html>
---
+++ publish.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Digitale</title>
+  <link rel="shortcut icon" href="images/logo.png">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
+  <link rel="stylesheet" href="theme.css" type="text/css"> </head>
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<body>
+  <nav class="navbar navbar-expand-md bg-primary navbar-dark">
+    <div class="container_header">
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar2SupportedContent" aria-controls="navbar2SupportedContent" aria-expanded="false"
+        aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+      <div class="collapse navbar-collapse text-center justify-content-end" id="navbar2SupportedContent">
+        <ul class="navbar-nav">
+          </li>
+          <li style="padding-right: 860px;"><a class="navbar-brand" href="#"><img src="images/logo.png" alt="" ></i><b> Digitale</b></a>
+          <li class="nav-item">
+            <a class="btn navbar-btn ml-2 text-white"><i class=""></i>  Download</a>
+          </li>
+          <li>
+            <a href="publish.php" class="btn navbar-btn ml-2 text-white"><i class=""></i> Publish</a>
+          </li>
+          <li>
+            <li data-toggle="modal" data-target="#mylogin"><a href="#" class="btn navbar-btn ml-2 text-white"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class="text-left py-5 " style="background-position: left center;">
+        <div class="py-5 ">
+  <div class="container">
+    <div class="row1">
+      <div class="col-md-8" >
+        <h1>Publish</h1>
+        <p>Buat ceritamu disini</p>
+        <form>
+          <div class="form-group"> <label for="InputName">Nama Publisher</label>
+            <input type="text" class="form-control" id="" placeholder=""> </div>
+          <div class="form-group"> <label for="InputJudul">Judul</label>
+            <input type="email" class="form-control" id="" placeholder=""> </div>
+          <div class="form-group"> <label for="Textarea">Sinopsis</label> <textarea class="form-control" id="Textarea" rows="3" placeholder=""></textarea> </div>
+          <div class="form-group"> <label for="InputPenulis">Penulis</label>
+            <input type="email" class="form-control" id="" placeholder=""> </div>
+            <div class="form-group"> <label for="InputEmail1">Ilustrator</label>
+              <input type="email" class="form-control" id="" placeholder=""> </div>
+              <div class="form-group"> <label for="InputEmail1">Editor</label>
+                <input type="email" class="form-control" id="" placeholder=""> </div>
+                <div class="form-group"> <label for="InputEmail1">Kateori</label>
+                  <input type="email" class="form-control" id="" placeholder=""> </div>
+          
+          <a href="upload.php" type="submit" class="btn btn-secondary">Submit</a>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+        <div>
+        </div>
+      </div>
+    </div>
+  </div>
